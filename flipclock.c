500b19c0dd5fab9b4a71e3799ec558ccbd9722f5
sdao <sdao@sfu.ca>
2023-03-14 23:14:54 -0700

    add accelerometer side detection logic

--- flipclock.c
+++ flipclock.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[]) {
     Accel_start_thread();
-    Clock_start_thread();
-    Clock_set_manual_time(8, 59);
+    //Clock_start_thread();
+    //Clock_set_manual_time(8, 59);
     while(1) {
-        // int* accel_values = Accel_get_values();
-        // printf("x: %d, y: %d, z: %d\n", accel_values[0], accel_values[1], accel_values[2]);
+        int* accel_values = Accel_get_values();
+        printf("x: %d, y: %d, z: %d\n", accel_values[0], accel_values[1], accel_values[2]);
         
-        Utils_sleep_for_ms(100);
+        Utils_sleep_for_ms(200);
     }
     Accel_stop_thread();
-    Clock_stop_thread();
+    //Clock_stop_thread();
 }
--- src/accelerometer.c
+++ src/accelerometer.c
@@ -29,8 +29,11 @@ int accel_values[3] = {0};
 
 typedef enum {
     X,
+    N_X,
     Y,
+    N_Y,
     Z,
+    N_Z,
     NONE,
 } Accel_direction;
 
@@ -38,6 +41,8 @@ typedef enum {
 static void* accel_thread_helper(void* arg) {
     int i2cFileDesc_BUS1 = Utils_initI2cBus(I2CDRV_LINUX_BUS1, I2C_DEVICE_ADDRESS);
     Utils_writeI2cReg(i2cFileDesc_BUS1, 0x20, 0x27);
+
+    Accel_direction prev_trig = NONE;
     while (running) {
         long long debounce_time = 25;
         unsigned char buff[BUFF_LEN];
@@ -45,9 +50,60 @@ static void* accel_thread_helper(void* arg) {
         int16_t x = (buff[1] << 8) | (buff[0]);
         int16_t y = (buff[3] << 8) | (buff[2]);
         int16_t z = (buff[5] << 8) | (buff[4]);
-        z -= 16200; // gravity offset
+        //z -= 15000; // gravity offset
 
         int new_values[3] = {x, y, z};
+        int threshold = 10000;
+        Accel_direction max_dir = NONE;
+        if (abs(x) >= abs(y)) {
+            if (abs(x) >= abs(z)) {
+                max_dir = X;
+            } else { max_dir = Z; }
+        } else if (abs(y) >= abs(z)) {
+            max_dir = Y;
+        } else {
+            max_dir = Z;
+        }
+
+        if ((x >= threshold) && max_dir == X) {
+            if (prev_trig == X) {
+                debounce_time = 200;
+                printf("FLIPPED X\n");
+            }
+            prev_trig = X;
+        } else if ((x <= -threshold) && max_dir == X) {
+            if (prev_trig == N_X) {
+                debounce_time = 200;
+                printf("FLIPPED -X\n");
+            }
+            prev_trig = N_X;
+        } else if ((y >= threshold) && max_dir == Y) {
+            if (prev_trig == Y) {
+                debounce_time = 200;
+                printf("FLIPPED Y\n");
+            }
+            prev_trig = Y;
+        } else if ((y <= -threshold) && max_dir == Y) {
+            if (prev_trig == N_Y) {
+                debounce_time = 200;
+                printf("FLIPPED -Y\n");
+            }
+            prev_trig = N_Y;
+        } else if ((z >= threshold) && max_dir == Z) {
+            if (prev_trig == Z) {
+                debounce_time = 200;
+                printf("FLIPPED Z\n");
+            }
+            prev_trig = Z;
+        } else if ((z <= -threshold) && max_dir == Z) {
+            if (prev_trig == N_Z) {
+                debounce_time = 200;
+                printf("FLIPPED -Z\n");
+            }
+            prev_trig = N_Z;
+        } else {
+            prev_trig = NONE;
+        }
         memcpy(accel_values, new_values, sizeof(new_values));
         //printf("[x] %d, [y] %d, [z] %d\n", x, y, z);
 
